6264f00677f8cc494083268c9fa5d15fdf9c3a8f
arifurrahmansajid <iamnobody3456@gmail.com>
2026-06-25 10:52:46 +0600

    update

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
